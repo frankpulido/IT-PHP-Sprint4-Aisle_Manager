70bf2e5153d47bc409df813d3dc8f12aba79e516
frankpulido <frankpulido@me.com>
2025-12-06 12:04:54 +0100

    fixing and relocating new route again

--- routes/web.php
+++ routes/web.php
@@ -183,6 +183,7 @@ Route::get('/sections', [SectionController::class, 'all']); // All sections rega
 
 // Solution : Laravel's Built-in Static Serving. Serve static assets directly (Ralway deployment)
 // Must be after debugging routes to avoid interference
+/*
 Route::get('/styles/{file}', function ($file) {
     $path = public_path('styles/' . $file);
     
@@ -208,6 +209,7 @@ Route::get('/styles/{file}', function ($file) {
     
     return response()->file($path, ['Content-Type' => $mime]);
 });
+*/
 
 /*
 GET
